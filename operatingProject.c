e13d97e0f407660c501f69689abe7c1da732626d
R Abhishek <43127616+abhi111q@users.noreply.github.com>
2019-03-22 00:37:04 +0530

    updated one
    updated project

--- operatingProject.c
+++ operatingProject.c
@@ -11,17 +11,23 @@ int main(){
 	struct OS o[10];
 	int i;
 	printf("WELCOME TO MY OPERATING SYSTEM PROGRAM \n\n");
-	
+	getchar();
 	for(i=0;i<10;i++){
+		system("cls");//hjjh
 		printf("\nEnter %dth name :",i+1);
 		scanf("%s",o[i].name);
 		printf("\nEnter %dth quantity number:",i+1);
 		scanf("%d",&o[i].quantity);
 	}
 	system("cls");
-	printf("Entered name and quantity are as follows::");
+	printf("Entered name and quantity are as follows::\n\n");
 	printf("Name \t\t Quantity");
 	for(i=0;i<10;i++){
 		printf("\n%s \t\t %d",o[i].name,o[i].quantity);
+	}
+	
+	printf("\nSorted by quantity number::\n\n");
+	for(i=0;i<10;i++){
+		printf("Abc");
 	}
 }
